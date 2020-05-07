232bb36c6fd27696a6a84696bc6526237a22477c
Kelly Gowing <Gowing.kelly@yahoo.com>
2020-05-06 19:33:56 -0700

    get current weather working in imperial

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Weather conditions https://openweathermap.org/weather-conditions
 // Api Documentation
+// Current https://openweathermap.org/current 
+// UV https://openweathermap.org/api/uvi 
 
 // Open with San Diego weather
 
@@ -7,29 +9,44 @@
 
 $(document).ready(function () {
 
-  var api = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=" + apiKey
-  var apiKey = "b0d36754b058f6a0166bf74f6a5ebe40"
+  
+  var apiKey = "b0d36754b058f6a0166bf74f6a5ebe40";
+  var city;
 
   $("#search-btn").on("click", function() {
     event.preventDefault();
     // Search for a city on click
-
+    city = $("#search").val().trim()
     
+    getWeather();
 
-    // Add city to list (if city != null)
+    // Add city to list (if city != null) ***
     addCity();
 
     function addCity () {
-      var search = $("#search").val()
-      $("#past-searches").prepend($("<button>").attr("type", "button").addClass("past text-muted list-group-item list-group-item-action").text(search))
+      $("#past-searches").prepend($("<button>").attr("type", "button").addClass("past text-muted list-group-item list-group-item-action").text(city))
       $("#search").val("")
     }
 
-    
-    
-
   });
 
+  function getWeather() {
+
+    var api = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
+
+    $.ajax({
+      url: api,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response)
+      $("#city").text(response.name);
+      $("#temp").text(`${response.main.temp} °F`);
+      $("#hum").text(`${response.main.humidity} %`);
+      $("#wind").text(`${response.wind.speed} MPH`);
+      $("#uv").text(``);
+
+    });
+  }
 
 
 });
